7e05b139800028c2dd9a904e1f97355e92a28fb7
aturant <a.turant@gmail.com>
2021-04-20 17:47:31 +0200

    task_2

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello World!"}
+
+@app.get("/method")
+def root(request: Request, response: Response):
+	if request.method in ['GET','PUT','OPTIONS','DELETE']:
+		response.status_code=200
+	elif request.method=='POST':
+		response.status_code=201
+	
+	return {"method": request.method}
