119a2beb1f277a2ecdac83cf1792e38f0563321e
aturant <a.turant@gmail.com>
2021-04-20 20:15:43 +0200

    next task

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request, Response
+from hashlib import sha512
 
 app = FastAPI()
 
@@ -12,13 +13,30 @@ def root():
 @app.put("/method")
 @app.options("/method")
 @app.delete("/method")
-@app.post("/method")
-def root(request: Request, response: Response):
-    if request.method in ['GET', 'PUT', 'OPTIONS', 'DELETE']:
-        response.status_code = 200
-        return {"method": request.method}
+@app.post("/method", status_code=201)
 
-    elif request.method == 'POST':
-        response.status_code = 201
-        return {"method": request.method}
-    
+def root(request: Request, response: Response):
+	return {"method": request.method}
+
+@app.get("/auth")
+def root(response: Response,
+	password: str,
+	password_hash:str):
+	
+	try:
+
+		m = sha512()
+		m.update(password.encode('utf-8'))
+		password_test_hash=str(m.hexdigest()).encode('utf-8')
+
+		if password_test_hash in str(password_hash).encode('utf-8'):
+			response.status_code=204
+		else:
+			response.status_code=401
+
+	except NameError:
+		response.status_code=666
+	return {
+			"password": password, 
+			"password_hash":password_hash, 
+			"new_hash":password_test_hash}
