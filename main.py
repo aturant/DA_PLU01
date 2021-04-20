7d43f4bac269082a22c7922fb9256999d2abfd67
aturant <a.turant@gmail.com>
2021-04-20 17:48:11 +0200

    pep8 additions

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from fastapi import FastAPI, Request, Response
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
     return {"message": "Hello World!"}
 
+
 @app.get("/method")
 def root(request: Request, response: Response):
-	if request.method in ['GET','PUT','OPTIONS','DELETE']:
-		response.status_code=200
-	elif request.method=='POST':
-		response.status_code=201
-	
-	return {"method": request.method}
+    if request.method in ['GET', 'PUT', 'OPTIONS', 'DELETE']:
+        response.status_code = 200
+    elif request.method == 'POST':
+        response.status_code = 201
+
+    return {"method": request.method}
