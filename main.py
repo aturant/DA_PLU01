59a7a1c2c7a3bf64ef4034b9585904b0c2c95679
aturant <a.turant@gmail.com>
2021-04-20 23:48:34 +0200

    sdcvsdv

--- main.py
+++ main.py
@@ -64,7 +64,9 @@ def root(
 
 @app.post('/register', status_code = 201)
 def root(in_item: InItem):
-
+	in_item.name.strip()
+	in_item.surname.strip()
+	
     app.client_id += 1
     in_item.id = app.client_id
     offset = len(in_item.name) + len(in_item.surname)
