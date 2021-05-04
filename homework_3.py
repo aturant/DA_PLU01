f9e90006026898ad7f752276e712f70bfd9e4190
aturant <a.turant@gmail.com>
2021-05-04 22:59:10 +0200

    dfd

--- homework_3.py
+++ homework_3.py
@@ -11,7 +11,7 @@ router = APIRouter()
 # security additions
 security = HTTPBasic()
 loginy={'4dm1n':'NotSoSecurePa$$'}
-session_ids=set()
+session_ids={"12334","4567"}
 
 @router.get("/hello", response_class=HTMLResponse)
 def hello():
@@ -31,7 +31,7 @@ def main(
 	):
 	global session_ids
 	print(session_ids)
-	
+
 	if session_token in session_ids:
 		return {"token": session_token} 
 
@@ -56,3 +56,46 @@ def main(
 			key="session_token", 
 			value=session_token)
 		return {"token": session_token} 
+
+
+###############################################################################
+# Skoro mamy już utworzoną sesję, czas na jej użycie. Dla metody GET, stwórz endpointy /welcome_session oraz /welcome_token. Oba endpointy mają wpuścić tylko zalogowanych użytkowników. W sytuacji błędnego klucza sesji lub jego braku, należy zwrócić kod HTTP 401. Pierwszy endpoint otrzyma klucz sesji w utworzonym cookie. Drugi endpoint otrzyma klucz w adresie url, poprzez parametr token. 
+#Po poprawnej walidacji klucza sesji, endpointy w zależności od otrzymanego (lub też nie) query parametru format w adresie url, mają zwrócić kod HTTP 200 oraz odpowiedną wiadomość z pasującym nagłówkiem content-type:
+# - format=json - należy zwrócić następującą wiadomość w formacie json: {"message": "Welcome!"}
+# - format=html - należy zwrócić dowolną wiadomość w formacie html, która ma zawierać następujący fragment: <h1>Welcome!</h1>
+# - w pozostałych przypadkach należy zwrócić wiadomość Welcome! w formacie plain
+
+
+@router.get("/welcome_session", status_code=200)
+
+#/welcome_token?token=verylongvalueXOXOXOXOXO&format=json
+
+@router.get("/welcome_token", status_code=200)
+def main(
+	token: str=None,
+	format: str="plain", 
+	session_token: str = Cookie(None),
+	response:Response ):
+
+	__mime_dict={
+					"json":"application/json",
+					"plain":"text/plain",
+					"html":"text/html"}
+
+	__token=token if token else session_token
+	__format=format
+	if __token not in session_ids:
+		raise HTTPException(status_code=401,detail="wrong password")
+
+	if format=="json":
+		message={"message": "Welcome!"}
+	
+	elif format=="html":
+		message=="<h1>Welcome!</h1>"
+	
+	else:
+		message="Welcome!"
+
+	response.media_type=__mime_dict[__format]
+	response.content=message
+	return message
